a36d6d26f0f60d0f17b43e1971da930de5855d17
ldsbernal <ldsbernal@gmail.com>
2024-10-21 01:03:07 +0000

    se le cambio el nombre de abajo al footer por el de sugas y el link redirecciona a sofia plus

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        SUGAS by
+        <a href="https://oferta.senasofiaplus.edu.co/sofia-oferta/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">SofiaPlus</a>
     </div>
 </template>
